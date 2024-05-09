f8207cf4e9f0d754abbb0af3281005950d5b40da
nitishranjan27 <nk47595@gmail.com>
2024-05-09 14:15:48 +0530

    Day 2 Answer of WPP

---
+++ WPP_day2ans/WPP_day2ans.js
@@ -0,0 +1,6 @@
+//Write a JavaScript program to print the current window contents.
+
+function printWindowContents() {
+    // window.print() --> This function triggers the browser to print functionality, allowing users to print the content of the current Page or Window.
+    window.print();
+}
